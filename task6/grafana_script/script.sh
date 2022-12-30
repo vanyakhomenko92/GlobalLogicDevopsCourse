e0094168002f78832a857c1fbda7c7326cf682e6
Ivan Khomenko <iamminer2017@gmail.com>
2022-12-30 10:13:55 -0500

    fixed typos

--- task6/grafana_script/script.sh
+++ task6/grafana_script/script.sh
@@ -3,6 +3,5 @@ sudo apt-get -y update
 wget https://script.technologyrss.com/linux/grafana/install-grafana.sh
 chmod 755 install-grafana.sh
 sudo bash install-grafana.sh
-sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server
