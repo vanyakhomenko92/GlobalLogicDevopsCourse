f23e7270c9cfba90c95568c40dc20a0370f4bc51
Ivan Khomenko <iamminer2017@gmail.com>
2022-12-30 13:46:34 -0500

    test

--- task6/providers.tf
+++ task6/providers.tf
@@ -1,16 +1,16 @@
 provider "aws"{
   # Please write your credentials ----------
-  access_key = "AKIA3IKVMAYWTAYSW6NN"
-  secret_key = "YBdm5Rdu8z18X/DXsSal5FgJ7Jkc43frWEvk3QH0"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
   #-----------------------------------------
 } 
 provider "azurerm" {
   features {}
   # Please write your credentials ----------
-  subscription_id = "97a77d59-8331-4eff-aab8-03103fb38088"
-  client_id       = "9dba45cb-f6e1-4bb4-8808-2ddb3c8a9e67"
-  client_secret   = "ONE8Q~qx8n8T4tgy4yEwkSxwmK_yscu4mXpm1c9U"
-  tenant_id       = "0a4d4529-3bd8-41d2-bdbe-aa4634a1752b"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
   #-----------------------------------------
 }
